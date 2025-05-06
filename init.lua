0b8e63ad674b0ec84f0601b844dd8ab2838779ed
nduruz <nduruz@groupemutuel.ch>
2025-05-06 10:36:03 +0200

    [S]earch all [F]iles

--- init.lua
+++ init.lua
@@ -483,6 +483,14 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+      -- Search all files, included hidden and gitignored ones
+      vim.keymap.set('n', '<leader>sF', function()
+        require('telescope.builtin').find_files {
+          hidden = true,
+          no_ignore = true,
+        }
+      end, { desc = '[S]earch all [F]iles (including hidden and gitignored)' })
+
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
