c07ebdf98e11a6ac8e2cb463e3521683b4df8265
nduruz <nduruz@groupemutuel.ch>
2025-08-05 12:14:54 +0200

    comment

--- init.lua
+++ init.lua
@@ -1049,10 +1049,8 @@ require('lazy').setup({
     event = 'BufEnter',
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
+  -- Colorscheme
+  {
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     -- 'folke/tokyonight.nvim',
     'catppuccin/nvim',
